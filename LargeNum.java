75a83ae2e376f644781cba5f01c7ab0a40dbc5b3
debarshigol <68940295+debarshigol@users.noreply.github.com>
2025-02-10 12:07:01 +0530

    Update LargeNum.java

--- LargeNum.java
+++ LargeNum.java
@@ -3,7 +3,23 @@ package Collections_Challenge;
 public class LargeNum {
 
 	public static void main(String[] args) {
-		String num = "132";
+		String num = "1329";
+		LargeOdd(num);
+		LargeEven(num);
+
+	}
+	public static void LargeEven(String num) {
+		String numMod = " ";
+		for (int i = num.length() - 1; i >= 0; i--) {
+			if (num.charAt(i) % 2 == 0) {
+				numMod = num.substring(0, i + 1);
+				break;
+			}
+		}
+		System.out.println("Largeest Even Number is: " + numMod);
+	}
+	
+	public static void LargeOdd(String num) {
 		String numMod = " ";
 		for (int i = num.length() - 1; i >= 0; i--) {
 			if (num.charAt(i) % 2 != 0) {
@@ -11,8 +27,9 @@ public class LargeNum {
 				break;
 			}
 		}
-		System.out.println("Largeest Number is: " + numMod);
+		System.out.println("Largeest Odd Number is: " + numMod);
 
 	}
 
 }
+
